bc2120daac89d4f8cb96f1ea305089f7812f410e
etyso <etysoft@yandex.ru>
2021-11-13 12:17:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/etysoft/cameraoverlay/MainActivity.java
@@ -0,0 +1,178 @@
+package ru.etysoft.cameraoverlay;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import com.otaliastudios.cameraview.BitmapCallback;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.PictureResult;
+import com.otaliastudios.cameraview.VideoResult;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
+        CameraView camera = findViewById(R.id.camera);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setProgress(50);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ((ImageView) findViewById(R.id.imgView)).setScaleType(ImageView.ScaleType.FIT_CENTER);
+               findViewById(R.id.imgView).setAlpha(Float.parseFloat(String.valueOf(progress / 100D)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar seekBar2 = findViewById(R.id.seekBar2);
+        seekBar2.setProgress((int) (camera.getZoom() * 100));
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                camera.setZoom(progress / 100.0F);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        camera.setLifecycleOwner(this);
+        camera.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(PictureResult result) {
+                // A Picture was taken!
+
+                 result.toBitmap(new BitmapCallback() {
+                    @Override
+                    public void onBitmapReady(@Nullable Bitmap bitmap) {
+
+                        MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "жопа" , "yourDescription");
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onVideoTaken(VideoResult result) {
+                // A Video was taken!
+            }
+
+            // And much more
+        });
+
+        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        Button buttonTakePic = (Button) findViewById(R.id.buttonTakePic);
+        buttonTakePic.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                camera.takePicture();
+            }
+        });
+    }
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Picasso.get().load(data.getData()).noPlaceholder().into((ImageView) findViewById(R.id.imgView));
+
+        ((ImageView) findViewById(R.id.imgView)).setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            //ImageView imageView = (ImageView) findViewById(R.id.imgView);
+           // imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
+    }
+}
